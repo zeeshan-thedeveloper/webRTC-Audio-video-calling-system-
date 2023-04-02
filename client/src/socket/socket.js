581d760ab45a96a9d439dd80c6619aea3451045e
Zeeshan <zeeshanahmedd0010@gmail.com>
2023-04-03 03:24:14 +0500

    done video calling

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
 
-var socket = io('http://localhost:3000', { transports : ['websocket'] });
+var socket = io('http://192.168.8.103:3000', { transports : ['websocket'] });
 
 export default socket;
